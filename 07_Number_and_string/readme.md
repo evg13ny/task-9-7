992d46588edccefd2c12a9bb045a7073de43d7bc
poliakov_evgenii <evg_pol@yahoo.com>
2020-12-14 01:26:22 +0300

    fcheck

---
+++ 07_Number_and_string/readme.md
@@ -0,0 +1,9 @@
+Simple web-based Calculator using Javascript.
+
+### Functions
+Addition, subtraction, multiplication, division, square root.
+
+### Features
++ history of operations
+
+To launch click the [link](https://evg13ny.github.io/task-9-7/07_Number_and_string/index.html).
--- 07_Number_and_string/script.js
+++ 07_Number_and_string/script.js
@@ -3,52 +3,79 @@ let operation = null;
 
 const inputWindow = document.getElementById('inputWindow');
 inputWindow.value = '0';
+const historyWindow = document.getElementById('historyWindow')
 
 
-document.getElementById('btn_clr').addEventListener('click', function () {
-    lastOperand = 0;
-    operation = null;
-    inputWindow.value = '0';
-})
-
-document.getElementById('btn_0').addEventListener('click', function () {
-    inputWindow.value += '0';
-})
+// buttons
 
 document.getElementById('btn_1').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '1';
 })
 
 document.getElementById('btn_2').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '2';
 })
 
 document.getElementById('btn_3').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '3';
 })
 
 document.getElementById('btn_4').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '4';
 })
 
 document.getElementById('btn_5').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '5';
 })
 
 document.getElementById('btn_6').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '6';
 })
 
 document.getElementById('btn_7').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '7';
 })
 
 document.getElementById('btn_8').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '8';
 })
 
 document.getElementById('btn_9').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '9';
+})
+
+document.getElementById('btn_0').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
+    inputWindow.value += '0';
 })
 
 document.getElementById('btn_dot').addEventListener('click', function () {
@@ -59,6 +86,14 @@ document.getElementById('btn_min').addEventListener('click', function () {
     inputWindow.value = (-1) * inputWindow.value;
 })
 
+document.getElementById('btn_clr').addEventListener('click', function () {
+    lastOperand = 0;
+    operation = null;
+    inputWindow.value = '0';
+})
+
+
+// functions
 
 document.getElementById('btn_sum').addEventListener('click', function () {
     lastOperand = parseFloat(inputWindow.value);
@@ -72,9 +107,9 @@ document.getElementById('btn_def').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
-document.getElementById('btn_multip').addEventListener('click', function () {
+document.getElementById('btn_multipl').addEventListener('click', function () {
     lastOperand = parseFloat(inputWindow.value);
-    operation = 'multip';
+    operation = 'multipl';
     inputWindow.value = '';
 })
 
@@ -93,35 +128,48 @@ document.getElementById('btn_squar').addEventListener('click', function () {
 
 document.getElementById('btn_calc').addEventListener('click', function () {
     if (operation === 'sum') {
-        const result = lastOperand + parseFloat(inputWindow.value);
+        const secondOperand = parseFloat(inputWindow.value);
+        const result = lastOperand + secondOperand;
+        historyWindow.value += lastOperand + ' + ' + secondOperand + ' = ' + result + '\n';
         lastOperand = null;
+        secondOperand = null;
         operation = 0;
         inputWindow.value = result;
     }
 
     if (operation === 'def') {
-        const result = lastOperand - parseFloat(inputWindow.value);
+        const secondOperand = parseFloat(inputWindow.value);
+        const result = lastOperand - secondOperand;
+        historyWindow.value += lastOperand + ' - ' + secondOperand + ' = ' + result + '\n';
         lastOperand = null;
+        secondOperand = null;
         operation = 0;
         inputWindow.value = result;
     }
 
-    if (operation === 'multip') {
-        const result = lastOperand * parseFloat(inputWindow.value);
+    if (operation === 'multipl') {
+        const secondOperand = parseFloat(inputWindow.value);
+        const result = lastOperand * secondOperand;
+        historyWindow.value += lastOperand + ' * ' + secondOperand + ' = ' + result + '\n';
         lastOperand = null;
+        secondOperand = null;
         operation = 0;
         inputWindow.value = result;
     }
 
     if (operation === 'split') {
-        const result = lastOperand / parseFloat(inputWindow.value);
+        const secondOperand = parseFloat(inputWindow.value);
+        const result = lastOperand / secondOperand;
+        historyWindow.value += lastOperand + ' / ' + secondOperand + ' = ' + result + '\n';
         lastOperand = null;
+        secondOperand = null;
         operation = 0;
         inputWindow.value = result;
     }
 
     if (operation === 'squar') {
         const result = Math.sqrt(lastOperand);
+        historyWindow.value += '√ ' + lastOperand + ' = ' + result + '\n';
         lastOperand = null;
         operation = 0;
         inputWindow.value = result;
